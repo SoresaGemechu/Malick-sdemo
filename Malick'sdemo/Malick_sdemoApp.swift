446a816d8e03e9266b05d351b1fb1bb6b3b20561
SoresaGemechu <Malickjibril@gmail.com>
2023-02-27 14:26:41 -0500

    Initial Commit

---
+++ Malick'sdemo/Malick_sdemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Malick_sdemoApp.swift
+//  Malick'sdemo
+//
+//  Created by Malick Gemechu on 2/27/23.
+//
+
+import SwiftUI
+
+@main
+struct Malick_sdemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
